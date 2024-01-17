7c7091301ebd908c6cf25980b951de1e3dc13909
Linghe-Wang <linghe567@gmail.com>
2024-01-17 15:31:10 -0600

    Upload starter code for monitoring page

--- modules/monitor.py
+++ modules/monitor.py
@@ -43,34 +43,33 @@ def load_file_meta_data(projectID):
         actions.append({"file": each["file"], "username": each['username'], "timestamp": each["timestamp"]})
 
 
-def generate(projectID, writer_action_idx):
-    text_array = []
-    diffs_htmls = []
-    users = []
-    data = collection.find({'project': projectID,
-                            "message": {"$ne": "assist"},
-                            "state": {"$ne": "user_selection"},
-                            'revision': {"$ne": []},
-                            })
-
-    for j in range(writer_action_idx, writer_action_idx + 2):
-        text_array.append(data[j]['text'])
-
-    diffs = dmp.diff_main(text_array[0]['text'], text_array[1]['text'])
-    dmp.diff_cleanupSemantic(diffs)
-    diffs_htmls.append(diff_prettyHtml(diffs))
-
-    info = []
-    print(len(users))
-    print(len(actions))
-    print(len(diffs_htmls))
-
-    for i in range(0, len(users)):
-        try:
-            if i == 0:
-                info.append({"users": users[i], "actions": actions[i], "htmls": actions[0]['text']})
-            if i > 0:
-                info.append({"users": users[i], "actions": actions[i], "htmls": diffs_htmls[i - 1]})
-        except:
-            break
-    return writer_action_idx, writer_action_idx + num_action_send, info
+# def generate(projectID, writer_action_idx):
+#     diffs = []
+#     diffs_htmls = []
+#     users = []
+#     data = collection.find({'project': projectID,
+#                             "message": {"$ne": "assist"},
+#                             "state": {"$ne": "user_selection"},
+#                             'revision': {"$ne": []},
+#                             })
+#
+#     for j in range(writer_action_idx, writer_action_idx + 2):
+#         diffs.append(data[j]['revision'])
+#
+#     dmp.diff_cleanupSemantic(diffs)
+#     diffs_htmls.append(diff_prettyHtml(diffs))
+#
+#     info = []
+#     print(len(users))
+#     print(len(actions))
+#     print(len(diffs_htmls))
+#
+#     for i in range(0, len(users)):
+#         try:
+#             if i == 0:
+#                 info.append({"users": users[i], "actions": actions[i], "htmls": actions[0]['text']})
+#             if i > 0:
+#                 info.append({"users": users[i], "actions": actions[i], "htmls": diffs_htmls[i - 1]})
+#         except:
+#             break
+#     return writer_action_idx, writer_action_idx + num_action_send, info
